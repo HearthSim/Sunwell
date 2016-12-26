e6a7f04503db3a34e50c3dac3a6c15c42c837a1e
Jerome Leclanche <jerome@leclan.ch>
2016-12-26 16:38:15 +0200

    Add webpack and typescript config files

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const webpack = require("webpack");
+
+
+var PROD = process.env.NODE_ENV === "production";
+
+module.exports = {
+	entry: {
+		sunwell: [path.join(__dirname, "src/sunwell.ts")],
+	},
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: PROD ? "[name].min.js" : "[name].js",
+		library: "Sunwell",
+		libraryTarget: "var",
+	},
+	node: {
+		canvas: "empty",
+		fs: "empty",
+		promise: "empty",
+	},
+	resolve: {
+		extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"],
+		exclude: [path.resolve(__dirname, "node_modules")],
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.ts$/,
+				loaders: [
+					"ts-loader",
+				],
+			},
+		],
+		noParse: [path.join(__dirname, "node_modules", "canvas")],
+	},
+	target: "web",
+	plugins: PROD ? [
+		new webpack.optimize.UglifyJsPlugin({minimize: true}),
+	] : [],
+};
