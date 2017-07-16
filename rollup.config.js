0558d4364930927f60443f81a4a5482b440bc08b
Mohamed Tedjani Meftah <mohamed-ted@live.com>
2017-07-16 14:09:23 +0100

    Clean up build targets and add cdn build

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,40 @@
-import typescript from "rollup-plugin-typescript"
-import resolve from "rollup-plugin-node-resolve"
-import commonjs from "rollup-plugin-commonjs"
-import jscc from "rollup-plugin-jscc"
-import cleanup from "rollup-plugin-cleanup"
+import typescript from "rollup-plugin-typescript";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+import jscc from "rollup-plugin-jscc";
+import cleanup from "rollup-plugin-cleanup";
+import uglify from "rollup-plugin-uglify";
 
+const ExternalModulesList = [].concat(
+	require("builtin-modules"),
+	Object.keys(require("./package.json").dependencies)
+);
 
-const ExternalModulesList = (() => {
-	return [].concat(
-		require("builtin-modules"),
-		Object.keys(require("./package.json").dependencies)
-	)
-})()
-
-const PLATFORM = process.env.BROWSER ? "web" : "node"
-
+const PLATFORM = ["web", "cdn", "node"].includes(process.env.PLATFORM) ? process.env.PLATFORM : "node";
+const PRODUCTION = !!process.env.PRODUCTION;
 export default {
 	entry: "src/Sunwell.ts",
-	format: PLATFORM == "web" ? "iife" : "cjs",
-	dest: `dist/sunwell.${PLATFORM}.js`,
+	format: PLATFORM == "cdn" ? "iife" : "cjs",
+	dest: `dist/sunwell.${PLATFORM}${PRODUCTION ? ".min" : ""}.js`,
 	external: ExternalModulesList,
 	moduleName: "Sunwell",
 	plugins: [
 		jscc({
 			values: {
-				_PLATFORM: PLATFORM
+				_PLATFORM: PLATFORM,
 			},
-			extensions: [".js", ".ts"]
+			extensions: [".js", ".ts"],
 		}),
 		typescript({
-			typescript: require("typescript")
+			typescript: require("typescript"),
 		}),
 		resolve(),
 		commonjs({
 			exclude: ExternalModulesList,
 			ignoreGlobal: true,
-			extensions: [".js", ".ts"]
+			extensions: [".js", ".ts"],
 		}),
-		cleanup()
-	]
-}
+		cleanup(),
+		PRODUCTION ? uglify() : undefined,
+	].filter(p => p),
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,51 @@
-const path = require('path')
-const webpack = require('webpack')
-const nodeExternals = require('webpack-node-externals')
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
 
-const PROD = process.env.NODE_ENV === 'production'
-const PLATFORM = process.env.PLATFORM || 'node'
+const PROD = process.env.NODE_ENV === "production";
+const PLATFORM = process.env.PLATFORM || "node";
 
-const TS_LOADER = { test: /\.ts$/, loaders: ['ts-loader'] }
+const TS_LOADER = {test: /\.ts$/, loaders: ["ts-loader"]};
 
 let plugins = [
-  new webpack.DefinePlugin({
-    'process.env.PLATFORM': JSON.stringify(PLATFORM)
-  })
-]
+	new webpack.DefinePlugin({
+		"process.env.PLATFORM": JSON.stringify(PLATFORM),
+	}),
+];
 
 // if (PROD) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: true,
-      mangle: false,
-      beautify: true
-    })
-  )
+plugins.push(
+	new webpack.optimize.UglifyJsPlugin({
+		compress: true,
+		mangle: false,
+		beautify: true,
+	})
+);
 //   plugins.push(new webpack.optimize.ModuleConcatenationPlugin())
 // }
 
 module.exports = [
-  {
-    name: 'sunwell',
-    target: PLATFORM,
-    entry: {
-      sunwell: path.join(__dirname, 'src/Sunwell.ts')
-    },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: PROD ? '[name].min.js' : '[name].js',
-      library: 'Sunwell',
-	//   libraryTarget: PLATFORM == 'web' ? 'var' : 'commonjs2'
-	  libraryTarget: 'umd',
-    umdNamedDefine: true
-    },
-    resolve: {
-      extensions: ['.webpack.js', '.web.js', '.ts', '.js']
-    },
-    externals: [nodeExternals()],
-    module: {
-      loaders: [TS_LOADER]
-    },
-    plugins
-  }
-]
+	{
+		name: "sunwell",
+		target: PLATFORM,
+		entry: {
+			sunwell: path.join(__dirname, "src/Sunwell.ts"),
+		},
+		output: {
+			path: path.resolve(__dirname, "dist"),
+			filename: PROD ? "[name].min.js" : "[name].js",
+			library: "Sunwell",
+			//   libraryTarget: PLATFORM == 'web' ? 'var' : 'commonjs2'
+			libraryTarget: "umd",
+			umdNamedDefine: true,
+		},
+		resolve: {
+			extensions: [".webpack.js", ".web.js", ".ts", ".js"],
+		},
+		externals: [nodeExternals()],
+		module: {
+			loaders: [TS_LOADER],
+		},
+		plugins,
+	},
+];
