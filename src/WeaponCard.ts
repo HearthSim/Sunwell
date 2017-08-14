c47496af4d6f3cba2c85324d4ce0e5c71b03df70
Andrew Burnett <andrew@andburn.info>
2017-08-14 19:11:33 +0100

    Replace basic weapon assets
    Closes #34.

--- src/WeaponCard.ts
+++ src/WeaponCard.ts
@@ -4,6 +4,14 @@ export default class WeaponCard extends Card {
 	public bodyTextColor = "white";
 	public bodyTextSize = {width: 470, height: 250};
 	public baseCardFrameAsset = "frame-weapon-";
+	public baseCardFrameCoords = {
+		sWidth: 527,
+		sHeight: 775,
+		dx: 80,
+		dy: 103,
+		dWidth: 527,
+		dHeight: 775,
+	};
 	public nameBannerAsset = "name-banner-weapon";
 	public baseRarityGemAsset = "rarity-weapon-";
 	public dragonAsset = null;
@@ -11,36 +19,43 @@ export default class WeaponCard extends Card {
 	public attackGemAsset = "attack-weapon";
 	public healthGemAsset = "durability";
 	public attackGemCoords = {
-		sWidth: 312,
-		sHeight: 306,
-		dx: 32,
-		dy: 906,
-		dWidth: 187,
-		dHeight: 183,
+		sWidth: 135,
+		sHeight: 133,
+		dx: 65,
+		dy: 753,
+		dWidth: 135,
+		dHeight: 133,
 	};
 	public attackTextCoords = {x: 118, y: 994};
 	public healthGemCoords = {
-		sWidth: 301,
-		sHeight: 333,
-		dx: 584,
-		dy: 890,
-		dWidth: 186,
-		dHeight: 205,
+		sWidth: 126,
+		sHeight: 140,
+		dx: 501,
+		dy: 744,
+		dWidth: 126,
+		dHeight: 140,
 	};
 	public healthTextCoords = {x: 668, y: 994};
 	public nameBannerCoords = {
-		sWidth: 660,
-		sHeight: 140,
-		dx: 56,
-		dy: 551,
-		dWidth: 660,
-		dHeight: 140,
+		sWidth: 514,
+		sHeight: 108,
+		dx: 87,
+		dy: 468,
+		dWidth: 514,
+		dHeight: 108,
 	};
-	public rarityGemCoords = {dx: 311, dy: 607};
+	public rarityGemCoords = {
+		sWidth: 96,
+		sHeight: 90,
+		dx: 302,
+		dy: 520,
+		dWidth: 96,
+		dHeight: 90,
+	};
 	public nameTextCurve = {
 		pathMiddle: 0.56,
-		maxWidth: 580,
-		curve: [{x: 10, y: 77}, {x: 50, y: 77}, {x: 500, y: 77}, {x: 570, y: 77}],
+		maxWidth: 450,
+		curve: [{x: 18, y: 56}, {x: 66, y: 56}, {x: 400, y: 56}, {x: 456, y: 56}],
 	};
 	public artCoords = {
 		sWidth: 384,
@@ -75,10 +90,10 @@ export default class WeaponCard extends Card {
 
 	public getWatermarkCoords() {
 		return {
-			dx: 264,
-			dy: 735,
-			dWidth: 256,
-			dHeight: 256,
+			dx: 241,
+			dy: 599,
+			dWidth: 220,
+			dHeight: 220,
 		};
 	}
 }
