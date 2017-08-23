b8ba4fcc25c20548ae48b698bdcddc2ff2078adf
Andrew Burnett <andrew@andburn.info>
2017-08-23 22:09:06 +0100

    Update Hero card rendering
    Closes #40.

--- src/HeroCard.ts
+++ src/HeroCard.ts
@@ -2,46 +2,106 @@ import Card from "./Card";
 
 export default class HeroCard extends Card {
 	public bodyTextColor = "black";
-	public bodyTextSize = {width: 490, height: 290};
+	public bodyTextCoords = {
+		dx: 143,
+		dy: 627,
+		dWidth: 376,
+		dHeight: 168,
+		sWidth: 376,
+		sHeight: 168,
+	};
 	public baseCardFrameAsset = "frame-hero-";
-	public baseRarityGemAsset = "rarity-hero-";
+	public baseCardFrameCoords = {
+		sWidth: 527,
+		sHeight: 795,
+		dx: 70,
+		dy: 87,
+		dWidth: 527,
+		dHeight: 795,
+	};
+	public baseRarityGemAsset = "rarity-";
 	public nameBannerAsset = "name-banner-hero";
 	public dragonAsset = "elite-hero";
-	public dragonCoords = null; // TODO
+	public dragonCoords = {
+		sWidth: 444,
+		sHeight: 298,
+		dx: 172,
+		dy: 40,
+		dWidth: 444,
+		dHeight: 298,
+	};
 	public attackGemAsset = null;
 	public healthGemAsset = "armor";
 	public attackGemCoords = null;
 	public attackTextCoords = null;
 	public healthGemCoords = {
-		sWidth: 92,
-		sHeight: 109,
-		dx: 603,
-		dy: 912,
-		dWidth: 147,
-		dHeight: 174,
+		sWidth: 115,
+		sHeight: 135,
+		dx: 498,
+		dy: 752,
+		dWidth: 115,
+		dHeight: 135,
 	};
-	public healthTextCoords = {x: 679, y: 1005};
+	public healthTextCoords = {x: 554, y: 822};
 	public nameBannerCoords = {
-		sWidth: 627,
-		sHeight: 156,
-		dx: 81,
-		dy: 535,
-		dWidth: 627,
-		dHeight: 156,
+		sWidth: 490,
+		sHeight: 122,
+		dx: 91,
+		dy: 458,
+		dWidth: 490,
+		dHeight: 122,
 	};
-	public rarityGemCoords = {dx: 327, dy: 607};
+	public rarityGemCoords = {dx: 311, dy: 529};
 	public nameTextCurve = {
-		pathMiddle: 0.56,
-		maxWidth: 520,
-		curve: [{x: 0, y: 135}, {x: 220, y: 42}, {x: 350, y: 42}, {x: 570, y: 125}],
+		pathMiddle: 0.5,
+		maxWidth: 420,
+		curve: [{x: 24, y: 98}, {x: 170, y: 36}, {x: 294, y: 36}, {x: 438, y: 96}],
 	};
+	public artCoords = {
+		sWidth: 346,
+		sHeight: 346,
+		dx: 161,
+		dy: 137,
+		dWidth: 346,
+		dHeight: 346,
+	};
+	public artClipPolygon = [
+		{x: 334, y: 134},
+		{x: 369, y: 143},
+		{x: 406, y: 164},
+		{x: 435, y: 187},
+		{x: 453, y: 213},
+		{x: 469, y: 245},
+		{x: 479, y: 270},
+		{x: 481, y: 290},
+		{x: 483, y: 332},
+		{x: 483, y: 380},
+		{x: 483, y: 438},
+		{x: 484, y: 485},
+		{x: 435, y: 473},
+		{x: 389, y: 467},
+		{x: 346, y: 465},
+		{x: 297, y: 466},
+		{x: 240, y: 473},
+		{x: 185, y: 486},
+		{x: 184, y: 445},
+		{x: 182, y: 357},
+		{x: 184, y: 302},
+		{x: 188, y: 271},
+		{x: 198, y: 240},
+		{x: 210, y: 217},
+		{x: 222, y: 198},
+		{x: 239, y: 178},
+		{x: 262, y: 160},
+		{x: 291, y: 145},
+	];
 
 	public getWatermarkCoords() {
 		return {
-			dx: 270,
-			dy: 735,
-			dWidth: 256,
-			dHeight: 256,
+			dx: 247,
+			dy: 625,
+			dWidth: 170,
+			dHeight: 170,
 		};
 	}
 }
