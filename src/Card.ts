8717acac3cf453b61765c758f61d778af3678b79
Jerome Leclanche <jerome@leclan.ch>
2018-03-27 01:21:34 +0300

    Move various helper functions to helpers.ts

--- src/Card.ts
+++ src/Card.ts
@@ -1,183 +1,21 @@
 import LineBreaker from "linebreak";
 import {CardClass, CardSet, CardType, MultiClassGroup, Race, Rarity} from "./Enums";
-import {cleanEnum} from "./helpers";
+import {
+	cleanEnum,
+	contextBoundingBox,
+	drawPolygon,
+	finishLine,
+	getCharDimensions,
+	getNumberStyle,
+	getPointOnCurve,
+	IPoint,
+} from "./helpers";
 import Sunwell from "./Sunwell";
 
 const CTRL_BOLD_START = "\x11";
 const CTRL_BOLD_END = "\x12";
 const CTRL_ITALIC_START = "\x13";
 const CTRL_ITALIC_END = "\x14";
-
-/**
- * Helper function to draw a polygon from a list of points.
- */
-function drawPolygon(context: CanvasRenderingContext2D, points: IPoint[], ratio: number): void {
-	if (points.length < 3) {
-		return;
-	}
-	context.beginPath();
-	// move to start point
-	context.moveTo(points[0].x * ratio, points[0].y * ratio);
-	// draw the lines starting at index 1
-	points.slice(1).forEach(pt => {
-		context.lineTo(pt.x * ratio, pt.y * ratio);
-	});
-	context.closePath();
-	context.stroke();
-}
-
-/**
- * Get the bounding box of a canvas content.
- * @returns {{x: *, y: *, maxX: (number|*|w), maxY: *, w: number, h: number}}
- */
-function contextBoundingBox(context: CanvasRenderingContext2D) {
-	const w = context.canvas.width;
-	const h = context.canvas.height;
-	const data = context.getImageData(0, 0, w, h).data;
-	let minX = 999;
-	let minY = 999;
-	let maxX = 0;
-	let maxY = 0;
-
-	let out = false;
-
-	for (let y = h - 1; y > -1; y--) {
-		if (out) {
-			break;
-		}
-		for (let x = 0; x < w; x++) {
-			if (data[y * (w * 4) + x * 4 + 3] > 0) {
-				maxY = Math.max(maxY, y);
-				out = true;
-				break;
-			}
-		}
-	}
-
-	if (maxY === undefined) {
-		return null;
-	}
-
-	out2: for (let x = w - 1; x > -1; x--) {
-		for (let y = 0; y < h; y++) {
-			if (data[y * (w * 4) + x * 4 + 3] > 0) {
-				maxX = Math.max(maxX, x);
-				break out2;
-			}
-		}
-	}
-
-	out3: for (let x = 0; x < maxX; x++) {
-		for (let y = 0; y < h; y++) {
-			if (data[y * (w * 4) + x * 4 + 3] > 0) {
-				minX = Math.min(x, minX);
-				break out3;
-			}
-		}
-	}
-
-	out4: for (let y = 0; y < maxY; y++) {
-		for (let x = 0; x < w; x++) {
-			if (data[y * (w * 4) + x * 4 + 3] > 0) {
-				minY = Math.min(minY, y);
-				break out4;
-			}
-		}
-	}
-
-	return {
-		x: minX,
-		y: minY,
-		maxX: maxX,
-		maxY: maxY,
-		w: maxX - minX,
-		h: maxY - minY,
-	};
-}
-
-function getNumberStyle(style: string) {
-	switch (style) {
-		case "-":
-			return "#f00";
-		case "+":
-			return "#0f0";
-		default:
-			return "white";
-	}
-}
-
-/**
- * Finishes a text line and starts a new one.
- */
-function finishLine(
-	bufferTextCtx: CanvasRenderingContext2D,
-	bufferRow: HTMLCanvasElement,
-	bufferRowCtx: CanvasRenderingContext2D,
-	xPos: number,
-	yPos: number,
-	totalWidth: number
-): [number, number] {
-	let xCalc = totalWidth / 2 - xPos / 2;
-
-	if (xCalc < 0) {
-		xCalc = 0;
-	}
-
-	if (xPos > 0 && bufferRow.width > 0) {
-		bufferTextCtx.drawImage(
-			bufferRow,
-			0,
-			0,
-			xPos > bufferRow.width ? bufferRow.width : xPos,
-			bufferRow.height,
-			xCalc,
-			yPos,
-			Math.min(xPos, bufferRow.width),
-			bufferRow.height
-		);
-	}
-
-	xPos = 5;
-	yPos += bufferRow.height;
-	bufferRowCtx.clearRect(0, 0, bufferRow.width, bufferRow.height);
-
-	return [xPos, yPos];
-}
-
-interface IPoint {
-	x: number;
-	y: number;
-	r?: number;
-}
-
-/**
- * Given a curve and t, the function returns the point on the curve.
- * r is the rotation of the point in radians.
- * @returns {{x: (number|*), y: (number|*), r: number}}
- */
-function getPointOnCurve(curve: IPoint[], t: number): IPoint {
-	const rX =
-		3 * Math.pow(1 - t, 2) * (curve[1].x - curve[0].x) +
-		6 * (1 - t) * t * (curve[2].x - curve[1].x) +
-		3 * Math.pow(t, 2) * (curve[3].x - curve[2].x);
-	const rY =
-		3 * Math.pow(1 - t, 2) * (curve[1].y - curve[0].y) +
-		6 * (1 - t) * t * (curve[2].y - curve[1].y) +
-		3 * Math.pow(t, 2) * (curve[3].y - curve[2].y);
-
-	const x =
-		Math.pow(1 - t, 3) * curve[0].x +
-		3 * Math.pow(1 - t, 2) * t * curve[1].x +
-		3 * (1 - t) * Math.pow(t, 2) * curve[2].x +
-		Math.pow(t, 3) * curve[3].x;
-	const y =
-		Math.pow(1 - t, 3) * curve[0].y +
-		3 * Math.pow(1 - t, 2) * t * curve[1].y +
-		3 * (1 - t) * Math.pow(t, 2) * curve[2].y +
-		Math.pow(t, 3) * curve[3].y;
-
-	return {x: x, y: y, r: Math.atan2(rY, rX)};
-}
 
 const RaceNames = {};
 RaceNames[Race.MURLOC] = {enUS: "Murloc"};
@@ -806,40 +644,12 @@ export default abstract class Card {
 			textContext.restore();
 		}
 
-		textContext.lineWidth = 10;
-		textContext.strokeStyle = "black";
 		textContext.lineCap = "round";
 		textContext.lineJoin = "round";
+		textContext.lineWidth = 10;
+		textContext.strokeStyle = "black";
 		textContext.textAlign = "left";
 		textContext.textBaseline = "middle";
-
-		// calculate text width
-		const getCharDimensions = () => {
-			const dim = [];
-			const em = textContext.measureText("M").width;
-			for (let i = 0; i < name.length; i++) {
-				textContext.save();
-				const char = name[i];
-				const scale = {x: 1, y: 1};
-				let charWidth = textContext.measureText(char).width + 0.1 * em;
-				switch (char) {
-					case " ":
-						charWidth = 0.2 * em;
-						break;
-					case "'": // see "Death's Bite"
-						charWidth = 0.27 * em;
-						scale.x = 0.5;
-						scale.y = 1;
-						break;
-				}
-				dim[i] = {
-					width: charWidth,
-					scale: scale,
-				};
-				textContext.restore();
-			}
-			return dim;
-		};
 
 		let fontSize = 45;
 		let dimensions = [];
@@ -847,7 +657,8 @@ export default abstract class Card {
 			fontSize -= 1;
 			textContext.font = fontSize + "px " + this.titleFont;
 		} while (
-			(dimensions = getCharDimensions()).reduce((a, b) => a + b.width, 0) > maxWidth &&
+			(dimensions = getCharDimensions(name, textContext)).reduce((a, b) => a + b.width, 0) >
+				maxWidth &&
 			fontSize > 10
 		);
 
